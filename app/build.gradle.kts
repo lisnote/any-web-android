68a457feb81e19649e014f9438e7fe162d0f0d4d
lisnote <lisnote@lisnote.com>
2025-05-23 13:06:30 +0800

    feat: 压缩安装包体积

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,6 +34,16 @@ android {
             )
         }
     }
+
+    splits {
+        abi {
+            isEnable = true
+            reset()
+            include("armeabi-v7a", "arm64-v8a", "x86", "x86_64")
+            isUniversalApk = false
+        }
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
